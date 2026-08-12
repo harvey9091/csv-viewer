dea8a818a4c5c04ce2b551f7cf029944bba4547f
harvey9091 <harveyspectrecode@gmail.com>
2026-08-12 19:44:24 +0300

    style(ui): redesign and enhance app UI with new styles and animations

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const server = http.createServer((req, res) => {
+    const filePath = path.join('D:\\Workspace\\csv-viewer', req.url === '/' ? 'index.html' : req.url);
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('Not found');
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+});
+server.listen(8765, () => console.log('Server running on http://localhost:8765'));
